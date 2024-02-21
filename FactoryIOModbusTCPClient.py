7c7012b0bc57ca8d60709c139338294457056520
Gunnar Storebø <gunnar.storebo@vlfk.no>
2024-02-21 11:17:47 +0100

    Lagt til funksjonlitet for bryter

--- FactoryIOModbusTCPClient.py
+++ FactoryIOModbusTCPClient.py
@@ -10,11 +10,17 @@ mbClient = ModbusClient(host='10.44.2.96', port=502, auto_open=True, debug=False
 
 while True:
     # 
-    factoryIOrunning = mbClient.read_discrete_inputs(0)[0]
-    endSensor = mbClient.read_discrete_inputs(1)[0]
-  
-    mbClient.write_single_coil(0, endSensor)
+    factoryIOrunning = mbClient.read_discrete_inputs(1)[0]
+    endSensor = mbClient.read_discrete_inputs(0)[0]
+    startButton = mbClient.read_discrete_inputs(2)[0]
+    
+    conveyor =  startButton and endSensor
+    startButtonLight = startButton
+   
+    mbClient.write_single_coil(1, startButtonLight)
+    mbClient.write_single_coil(0, conveyor)
     print('factoryIOrunning: ' + str(factoryIOrunning) + ' endSensor: ' + str(endSensor ))
+    print('startButton: ' + str(startButton ) + ' conveyor: ' + str(conveyor) )
 
     # sleep 1s before next polling
-    time.sleep(1)
+    time.sleep(0.1)
