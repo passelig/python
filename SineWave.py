e1069c3c5ff536be05779fcac250805212ff4faf
Gunnar Storebo <gunnar.storebo@gmail.com>
2024-05-02 13:08:03 +0200

    roboticstoolbox_test.py

--- SineWave.py
+++ SineWave.py
@@ -2,11 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Generate x values from 0 to 4*pi
-# x = np.linspace(0, 2 * np.pi, 10)
+x = np.linspace(0, 2 * np.pi, 10)
+y = np.sin(x)
+
+
 x = [0,0.698132,1.39626,2.0944,2.79253,3.49066,4.18879,4.88692,5.58505,6.28319]
 
 # Calculate y values (sine of x)
-y = np.(x)
+
 
 # Plot the sine wave
 plt.plot(x, y)
