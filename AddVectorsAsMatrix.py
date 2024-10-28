cabbcb7f4556270cb3b4d6fff63ebdef633b8aa8
Gunnar Storebo <gunnar.storebo@gmail.com>
2024-10-28 15:05:16 +0100

    Added error in AddVectorsAsMatrix.py

--- AddVectorsAsMatrix.py
+++ AddVectorsAsMatrix.py
@@ -4,30 +4,39 @@ import matplotlib.pyplot as plt
 class Vector:
   def __init__(self,x=0,y=0):
     self.matrix = np.array([[x],
-                            [y]])
-    self.alfaRad = np.arctan2(y,x)
-    self.alfaDeg = np.rad2deg(self.alfaRad) 
-    self.x = self.matrix[0][0]
-    self.y = self.matrix[1][0]
+                           [y]])
+  @property
+  def x(self):
+    return self.matrix[0][0]
+  @property
+  def y(self):
+    return  self.matrix[1][0]
+  @property
+  def angleDeg(self):
+    return  np.rad2deg(self.angleRad)
+  @property
+  def angleRad(self):
+    return np.arctan2(self.y,self.x)
+
 
 def plot_vectors(vectors):
     # plots a list of vectors
     # initialize start point 
     sumVector = Vector()
+    sumVector.matrix
     for vector in vectors:
-        endX = sumVector.x + vector.x
-        endY = sumVector.y + vector.y 
-        plt.plot([sumVector.x , endX], [sumVector.y,endY], label=f'Length:  Angle: {vector.alfaDeg:.2f} degrees')
-        # save endpoints before plotting the next vector
-        sumVector= Vector(x=endX, y=endY)
+        endX = sumVector.x + vector.x # save endpoints before plotting the next vector
+        endY = sumVector.y + vector.x
+        plt.plot([sumVector.x , endX], [sumVector.y,endY], 
+                 label=f'Length:  Angle: {vector.angleDeg:.2f} degrees')
+        sumVector.matrix += vector.matrix
     # finally plot the vector sum
     plt.plot([0,sumVector.x],[0,sumVector.y])
 
-vector1 = Vector(x=3,y=7 )  # Line 1 with length 3 and angle 30 degrees
-vector2 = Vector( x=2,y=-3)  # Line 2 with length 4 and angle 120 degrees
-vectorlist =[vector1, vector2]
-
-print(vector1.matrix[1][0])
+vector1 = Vector(x=3,y=7 )  
+vector2 = Vector( x=3,y=0)  
+vector3 = Vector( x=2,y=-2) 
+vectorlist =[vector1, vector2,vector3]
 
 # Plot lines
 plt.figure()
